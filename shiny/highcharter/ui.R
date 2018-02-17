d1bcacde15c64ef397915cfccf98a5f822fd4169
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-02-17 23:57:08 +0100

    add interval dropdown

--- shiny/highcharter/ui.R
+++ shiny/highcharter/ui.R
@@ -2,10 +2,11 @@ library(shiny)
 library(highcharter)
 
 fluidPage(
-    titlePanel("The great ETH analysis engine"),
+    titlePanel("The great BTC & ETH analysis engine"),
     sidebarLayout(
         sidebarPanel(
-            selectInput('symbol', 'Symbol', c('ETH', 'BTC'))
+            selectInput('symbol', 'Symbol', c('ETH', 'BTC')),
+            selectInput('interval', 'Interval', binancer:::BINANCE$INTERVALS)
         ),
         mainPanel(highchartOutput('plot'))
     )
